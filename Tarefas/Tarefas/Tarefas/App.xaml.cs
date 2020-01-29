ff70bded48f80fae19ff2134a482134f848ae7a7
Diogo Freitas <diogo.freitas@hotmail.com>
2020-01-29 20:54:37 -0300

    Start

---
+++ Tarefas/Tarefas/Tarefas/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarefas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Telas.Inicio());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
